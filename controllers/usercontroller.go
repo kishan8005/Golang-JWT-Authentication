1386541e3969b471bd6693aca7236f9c5ae41b59
kishan8005 <kishanm8005@gmail.com>
2025-11-11 00:13:10 +0530

    Mongo DB database connection

--- controllers/usercontroller.go
+++ controllers/usercontroller.go
@@ -1,9 +1,32 @@
 package controller
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/kishan8005/Golang-JWT-Authentication/database"
+	helper "github.com/kishan8005/Golang-JWT-Authentication/helpers"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword() {}
+
+func VerifyPassword() {}
+
 func Signup() {}
 
 func Login() {}
 
 func GetUsers() {}
 
-func GetUser() {}
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(ctx, userId); err != nil {
+
+		}
+	}
+}
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,23 @@
 package helper
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(ctx *gin.Context, role string) (err error) {
+	return err
+}
+
+func MatchUserTypeToUid(ctx *gin.Context, userId string) (err error) {
+	userType := ctx.GetString("user_type")
+	uid := ctx.GetString("uid")
+
+	if userType == "USER" && uid != userId {
+		err := errors.New("Unauthorised to access this resource")
+		return err
+	}
+	err = CheckUserType(ctx, userType)
+	return err
+}
